b712848fc9fa1c7b5bdf823f9438680519018ae5
Rafael Lima <rafaellimareis01@gmail.com>
2017-06-15 15:21:15 -0300

    Login de users não admin

--- app/Http/Routes/App.php
+++ app/Http/Routes/App.php
@@ -1,11 +1,8 @@
 <?php
-Route::get('login', 'AuthController@login');
-Route::post('login', 'AuthController@postLogin');
-Route::post('login', 'AuthController@logout');
-
+Route::get('login',['as' => 'getLogin', 'uses' => 'AuthController@login']);
+Route::get('logout',['as' => 'logout', 'uses' => 'AuthController@logout']);
+Route::post('login',['as' => 'postLogin', 'uses' => 'AuthController@postLogin']);
 
 Route::group(['middleware' => 'auth'], function(){
-    Route::get('/home', function(){
-        return view('app.layouts.app');
-    })->name('home');
+   Route::get('/home',['as' => 'home', 'uses' => 'HomeController@index']);
 });
---
+++ resources/views/app/home.blade.php
@@ -0,0 +1,9 @@
+@extends('app.layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      <p>Home</p>
+    </div>
+</div>
+@endsection
