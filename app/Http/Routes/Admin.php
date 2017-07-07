2f7176005e1a4f53396199d392da60740316142b
RafaelLimaReis <rafaellimareis@gmail.com>
2017-07-07 20:50:59 -0300

    Alteraçoes basicas

--- app/Http/Routes/Admin.php
+++ app/Http/Routes/Admin.php
@@ -7,9 +7,6 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/home',['as' => 'home', 'uses' => 'HomeController@index']);
 
     Route::resource('User', 'UsersController', ['only' => ['create','store','index','show']]);
-<<<<<<< HEAD
-    Route::resource('Area', 'AreasController', ['only' => ['create','store','index','show']]);
-=======
     Route::resource('Area', 'AreasController');
->>>>>>> 741c0dcf43724272020119493ffc525cf2d4c83b
+
 });
