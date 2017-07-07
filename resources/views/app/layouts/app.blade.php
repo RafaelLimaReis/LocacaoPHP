21e9cfc62a29eb94ed7a9b44b76d3deb64e7bd57
Rafael Lima <rafaellimareis01@gmail.com>
2017-07-07 11:37:56 -0300

    Mudança de theme

--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -4,7 +4,7 @@
          <meta charset="UTF-8"/>
         <title>Locação | @yield('title', 'App')</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'/>
-        <link rel="stylesheet" type="text/css" href="{!!asset('css/admin.min.css')!!}">
+        <link rel="stylesheet" type="text/css" href="{!!asset('css/admin.css')!!}">
 
     </head>
     <body id="nifty-ready">
