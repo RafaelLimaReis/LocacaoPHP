96bf926b07b0e5ed1fbfb035bb4d2e32507e2c13
Rafael Lima <rafaellimareis01@gmail.com>
2017-07-21 15:13:19 -0300

    Novas rotas

--- app/Http/Routes/Admin.php
+++ app/Http/Routes/Admin.php
@@ -1,12 +1,12 @@
 <?php
-Route::get('login',['as' => 'getLogin', 'uses' => 'AuthController@login']);
-Route::get('logout',['as' => 'logout', 'uses' => 'AuthController@logout']);
-Route::post('login',['as' => 'postLogin', 'uses' => 'AuthController@postLogin']);
+Route::get('login', ['as' => 'getLogin', 'uses' => 'AuthController@login']);
+Route::get('logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
+Route::post('login', ['as' => 'postLogin', 'uses' => 'AuthController@postLogin']);
 
-Route::group(['middleware' => 'auth'], function(){
-    Route::get('/home',['as' => 'home', 'uses' => 'HomeController@index']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
+    Route::get('/reserves', ['as' => 'reserves', 'uses' => 'ReserveCController@index']);
 
     Route::resource('User', 'UsersController');
     Route::resource('Area', 'AreasController');
-
 });
